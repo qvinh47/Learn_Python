032332e4d91e88f424add25a6cfe6e841f7ae3c1
qvinh47 <quangvinhdang47>
2023-04-05 14:53:39 +0700

    Python

--- HowKteam_LTPython/Day5_ListP2_LTP.py
+++ HowKteam_LTPython/Day5_ListP2_LTP.py
@@ -20,13 +20,27 @@
 # print(friends[0][0])
 # print(friends[1][1])
 
-#       #2. Copy List
-# lst1= [1,3,5]
+# # 2. Copy List
+# lst1 = [1, 3, 5]
 # # lst2 = lst1       #1
 # # Toán tử is: kiểm tra cái id (vị trí bộ nhớ của cái list đó) xem id có bằng nhau không
-# #Copy
-# lst2 = lst1.copy()  #s
-# print(lst1 is lst2)     #1 True #2 False(vì đã copy nên không phải bản gốc ~~)
+# # Copy
+# lst2 = lst1.copy()  # s
+# print(lst1 is lst2)  # 1 True #2 False(vì đã copy nên không phải bản gốc ~~)
 # print(lst1 == lst2)
-print("toi bi dien")
-# Khi nao moi het ngao
+
+#    3. List slicing : lấy ra 1 phần từ cái list ban đầu
+#    0  1   2    3    4
+#   -5 -4  -3   -2   -1
+a = [1, 3, 101, 111, 23]
+# new_lst = a[0:2:1] # Lấy khoảng [0:2) bước nhảy là 1
+# new_lst = a[:2]    # hoặc viết như thế này
+# print(new_lst is a)
+# new_lst = a[1:] # lấy từ 1 đến hết
+# new_lst = a[1:-1]
+# new_lst = a[-1:-4] # Sao chép ra toàn bộ phần  tử của list
+# print(new_lst)
+# Muốn lật lại list thì [::-1]
+new_lst = a[::-1]
+print(new_lst)
+# Rev thì nó sẽ thực thi trên cái list cũ, còn ::-1 thì sẽ in ra list mới
