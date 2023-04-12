f8ffffc3461cedf15590268f33b8d76619fe0d02
qvinh47 <quangvinhdang47>
2023-04-12 16:38:35 +0700

    Python

--- HowKteam_LTPython/Day4_List.py
+++ HowKteam_LTPython/Day4_List.py
@@ -1,43 +1,44 @@
-# numbers = [1, 2, 3, 4, 5, 1]
-# # number.remove(1)
-# # Lay gia tri cuoi ra
-# # last_value = numbers.pop()
-# # print(last_value)
-# # numbers[0] = 100
-# # numbers.extend([7, 8, 9])
-# # #dem so luong gia tri 1 trong list
-# # soluong = numbers.count(1)
-# # print(soluong)
-# # numbers.clear()
-# # print(numbers)
-# # chieu_dai = len(numbers)
-# # print(chieu_dai)
+numbers = [1, 2, 3, 4, 5, 1]
+numbers.remove(1)
+#Lay gia tri cuoi ra
+last_value = numbers.pop()
+print(last_value)
+numbers[0] = 100
+numbers.extend([7, 8, 9])
+#dem so luong gia tri 1 trong list
+soluong = numbers.count(1)
+print(soluong)
+numbers.clear()
+print(numbers)
+chieu_dai = len(numbers)
+print(chieu_dai)
 
-# # #chen
-# # numbers.insert(0,9998)
-# # print(numbers)
-# # numbers.insert(-1110,9998)
-# # print(numbers)
-# # numbers.insert(+1110,9998)
-# # print(numbers)
+#chen
+numbers.insert(0,9998)
+print(numbers)
+numbers.insert(-1110,9998)
+print(numbers)
+numbers.insert(+1110,9998)
+print(numbers)
 
-# # index
-# index = numbers.index(3)
-# print(index)
+# index
+index = numbers.index(3)
+print(index)
 
-# numbers.reverse()
-# print(numbers)
+numbers.reverse()
+print(numbers)
 
-# #Sap xep
-# numbers.sort(reverse=True)
-# print(numbers)
+#Sap xep
+numbers.sort(reverse=True)
+print(numbers)
 
-# friends = ["Quang", "Vinh", "Dang", "Bao"]
-# vinh = ["huy"]
-# del friends[1]
-# print(friends)
-# friends.extend(["Jack"])
-# print(friends)
+friends = ["Quang", "Vinh", "Dang", "Bao"]
+vinh = ["huy"]
+del friends[1]
+print(friends)
+friends.extend(["Jack"])
+print(friends)
+
 numbers = [12, 34, 56, -100, 67]
 max_value = max(numbers)
 print(max_value)
